82cc80cacadc4058faf7bc122b70eef6664d9b1a
safaa abdelkhalek <safaa3bdelkhaleq@gmail.com>
2026-06-25 19:11:30 +0300

    -

--- coz-core/src/environments/environment.development.ts
+++ coz-core/src/environments/environment.development.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: false,
-  //apiUrl: 'https://cozy-core-backend.vercel.app'
-  apiUrl: 'http://localhost:5000'
+  apiUrl: 'https://cozy-core-backend.vercel.app'
 };
--- coz-core/src/environments/environment.ts
+++ coz-core/src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  // apiUrl: 'https://cozy-core-backend.vercel.app'
-  apiUrl: 'http://localhost:5000'
+  apiUrl: 'https://cozy-core-backend.vercel.app'
 };
