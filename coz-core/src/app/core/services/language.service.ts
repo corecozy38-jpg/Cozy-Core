44ccedcd92259214fb159d389863cc77e0075d98
safaa abdelkhalek <safaa3bdelkhaleq@gmail.com>
2026-07-04 17:37:42 +0300

    handle angular version

--- coz-core/src/app/core/services/language.service.ts
+++ coz-core/src/app/core/services/language.service.ts
@@ -1,32 +1,36 @@
-import { computed, Injectable } from '@angular/core';
+import { computed, Injectable, Signal } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
-import { TranslateService } from '@ngx-translate/core';
+import { TranslateService, LangChangeEvent } from '@ngx-translate/core';
 import { BehaviorSubject, map, startWith } from 'rxjs';
+
 @Injectable({
   providedIn: 'root',
 })
 export class LanguageService {
   private currentLangSubject = new BehaviorSubject<string>('en');
   public currentLang$ = this.currentLangSubject.asObservable();
-  lagSignal:any;
+  lagSignal: Signal<string>;
 
   constructor(private translate: TranslateService) {
     const savedLang = localStorage.getItem('lang') || 'en';
     this.currentLangSubject.next(savedLang);
 
-    this.lagSignal = toSignal(this.translate.onLangChange.pipe(
-      startWith({ lang: this.translate.getCurrentLang() }),
-      map(e => e.lang)
-    ),
-      { initialValue: this.translate.getCurrentLang() }
-    )
-}
-    t(key:string){
-      return computed(()=>{
-          this.lagSignal();
-          return this.translate.instant(key);
-      })
-    }
+    this.lagSignal = toSignal(
+      this.translate.onLangChange.pipe(
+        startWith({ lang: savedLang } as LangChangeEvent),
+        map((e: LangChangeEvent) => e.lang)
+      ),
+      { initialValue: savedLang }
+    );
+  }
+
+  t(key: string) {
+    return computed(() => {
+      this.lagSignal();
+      return this.translate.instant(key);
+    });
+  }
+
   setLanguage(lang: string) {
     this.translate.use(lang);
     localStorage.setItem('lang', lang);
@@ -38,7 +42,6 @@ export class LanguageService {
   }
 
   getCurrentLang(): string {
-    return this.currentLangSubject.value;
+    return this.currentLangSubject.value; 
   }
-
 }
