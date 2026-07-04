17986715e79e186e1e7e317bc25d814ea2754f73
safaa abdelkhalek <safaa3bdelkhaleq@gmail.com>
2026-07-04 17:16:19 +0300

    handle translation

--- coz-core/src/app/app.config.ts
+++ coz-core/src/app/app.config.ts
@@ -1,12 +1,13 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, inject, provideAppInitializer, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withViewTransitions } from '@angular/router';
 import { routes } from './app.routes';
-import { provideTranslateService } from '@ngx-translate/core';
+import { provideTranslateService, TranslateService } from '@ngx-translate/core';
 import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { guestInterceptor } from './core/interceptors/guest-interceptor';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { authInterceptor } from './core/interceptors/auth-interceptor';
+import { firstValueFrom } from 'rxjs';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -20,6 +21,16 @@ export const appConfig: ApplicationConfig = {
         prefix: 'i18n/',
         suffix: '.json',
       })
+    }),
+    provideAppInitializer(() => {
+      const translate = inject(TranslateService);
+      const savedLang = localStorage.getItem('lang') || 'en';
+
+      const dir = savedLang === 'ar' ? 'rtl' : 'ltr';
+      document.documentElement.setAttribute('dir', dir);
+      document.documentElement.setAttribute('lang', savedLang);
+
+      return firstValueFrom(translate.use(savedLang));
     }),
   ]
 };
--- coz-core/src/app/core/services/language.service.ts
+++ coz-core/src/app/core/services/language.service.ts
@@ -2,7 +2,6 @@ import { computed, Injectable } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { TranslateService } from '@ngx-translate/core';
 import { BehaviorSubject, map, startWith } from 'rxjs';
-import enTranslations from "../../../../public/i18n/en.json"
 @Injectable({
   providedIn: 'root',
 })
@@ -12,16 +11,16 @@ export class LanguageService {
   lagSignal:any;
 
   constructor(private translate: TranslateService) {
-    this.translate.setTranslation('en',enTranslations);
     const savedLang = localStorage.getItem('lang') || 'en';
-    this.setLanguage(savedLang);
+    this.currentLangSubject.next(savedLang);
+
     this.lagSignal = toSignal(this.translate.onLangChange.pipe(
-    startWith({ lang: this.translate.getCurrentLang() }),
-    map(e => e.lang)
-  ),
-    { initialValue: this.translate.getCurrentLang() }
-  )
-  }
+      startWith({ lang: this.translate.getCurrentLang() }),
+      map(e => e.lang)
+    ),
+      { initialValue: this.translate.getCurrentLang() }
+    )
+}
     t(key:string){
       return computed(()=>{
           this.lagSignal();
